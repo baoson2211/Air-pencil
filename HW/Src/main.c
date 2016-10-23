7ae87159b68b014fae90fafe91e8741264829ffb
baoson2211 <baoson92@gmail.com>
2016-10-23 23:59:59 +0700

    Configures the SysTick to generate an interrupt each 1 microsecond Tested ADXL345 communication Remove freertos example

--- HW/Src/main.c
+++ HW/Src/main.c
@@ -1,36 +1,5 @@
-/**
-  ******************************************************************************
-  * @file    GPIO/IOToggle/main.c
-  * @author  MCD Application Team
-  * @version V3.5.0
-  * @date    08-April-2011
-  * @brief   Main program body.
-  ******************************************************************************
-  * @attention
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
-  ******************************************************************************
-  */
-
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f10x.h"
-#include "stm32f10x_it.h"
-#include "stm32f10x_conf.h"
-
-/* Scheduler includes --------------------------------------------------------*/
-#include "FreeRTOS.h"
-#include "task.h"
-#include "list.h"
-
-/* Sensors includes --------------------------------------------------------*/
-#include "ADXL345.h"
+#include "main.h"
 
 /** @addtogroup STM32F10x_StdPeriph_Examples
   * @{
@@ -44,18 +13,7 @@
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-TaskHandle_t T1_handle;
-TaskHandle_t T2_handle;
-TaskHandle_t T3_handle;
-const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
-
 /* Private function prototypes -----------------------------------------------*/
-void GPIO_Configuration(void);
-static void prvSetupHardware( void );
-static void Led1(void *pvParameters);
-static void Led2(void *pvParameters);
-static void Led3(void *pvParameters);
-
 /* Private functions ---------------------------------------------------------*/
 
 /**
@@ -65,77 +23,95 @@ static void Led3(void *pvParameters);
   */
 int main(void)
 {
-  /*!< At this stage the microcontroller clock setting is already configured,
+  GPIO_InitTypeDef GPIO_InitStructure;
+  uint32_t tick;
+  char str[50];
+
+  /*!< At this stage the microcontroller clock setting is already configured, 
        this is done through SystemInit() function which is called from startup
        file (startup_stm32f10x_xx.s) before to branch to application main.
        To reconfigure the default setting of SystemInit() function, refer to
        system_stm32f10x.c file
      */
+  /* USART1 Configuration */
+  USART_Configuration();
 
-  /* To achieve GPIO toggling maximum frequency, the following  sequence is mandatory.
-     You can monitor PC1 or PC3 or PC13 on the scope to measure the output signal.
-     If you need to fine tune this frequency, you can add more GPIO set/reset
-     cycles to minimize more the infinite loop timing.
-     This code needs to be compiled with high speed optimization option.  */
+  /* I2C2 Configuration */
+  I2C_Configration(I2C2, I2C_FAST_SPEED);
+  USART_SendText(USART1, "I2C config done\n\r");
 
-  NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );  //Needed for STM32F10x
-  GPIO_Configuration();
+  /* ADXL345 Initialize */
+  if (ADXL345_Initialize(I2C2)) {
+    while(1);
+  }
+  USART_SendText(USART1, "ADXL345 Initialize done\n\r");
+  sprintf(str, "Data format: 0x%x\n\r", (( ADXL345_FULL_RES | ADXL345_RANGE(ADXL345_RANGE_PM_16G) ) & 0xFF ));
+  USART_SendText(USART1, str);
+  sprintf(str, "Bandwidth rate: 0x%x\n\r", ( ADXL345_RATE(0x0C) & 0xFF ));
+  USART_SendText(USART1, str);
+  sprintf(str, "Power control: 0x%x\n\r", ( ADXL345_PCTL_MEASURE & 0xFF ));
+  USART_SendText(USART1, str);
+  sprintf(str, "Interrupt: 0x%x\n\r", (ADXL345_DATA_READY & 0xFF ));
+  USART_SendText(USART1, str);
 
-  xTaskCreate( Led1, "Task1", 128, NULL, 3, &T1_handle);
-  xTaskCreate( Led2, "Task2", 128, NULL, 2, &T2_handle);
-  xTaskCreate( Led3, "Task3", 128, NULL, 1, &T3_handle);
-  vTaskStartScheduler();
-}
-
-static void prvSetupHardware( void )
-{
-  SystemInit();
-  /* Configure HCLK clock as SysTick clock source. */
-  SysTick_CLKSourceConfig( SysTick_CLKSource_HCLK );
-}
-
-void GPIO_Configuration(void) {
-  GPIO_InitTypeDef GPIO_InitStructure;
   /* GPIOC Periph clock enable */
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
-  /* Configure PC1, PC3 and PC13 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_3 | GPIO_Pin_13;
+  /* Configure PC1 and PC3 in output pushpull mode */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_3;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-  /* Configure SysTick Timer */
-  if (SysTick_Config(SystemCoreClock / 1000))
+  SystemInit();
+  if (SysTick_Config(SystemCoreClock / 1000000))
+  {
+    /* Capture error */
     while (1);
-}
+  }
+  USART_SendText(USART1, "SysTick config done\n\r");
 
-static void Led1(void *pvParameters) // This is Task1
-{
-  while(1){
-    // toggle led
-    GPIOC->ODR ^= 0x02;
-    vTaskDelay(2000);
+
+  while (1)
+  {
+    tick = micros();
+    uint16_t x_axis = get_RawAccel_X(I2C2);
+    uint16_t y_axis = get_RawAccel_Y(I2C2);
+    uint16_t z_axis = get_RawAccel_Z(I2C2);
+    tick = micros()- tick;
+    GPIOC->ODR ^= 0x0A;
+    //DelayUs(1000000);
+    //for(int i = 0 ; i < 500000; i++);
+
+    sprintf(str,"%ldus %x - %x - %x\n\r", tick, x_axis, y_axis, z_axis);
+    USART_SendText(USART1, str);
   }
 }
 
-
-static void Led2(void *pvParameters)
+void DelayUs(__IO uint32_t nTime)
 {
-  while(1){
-    // toggle led
-    GPIOC->ODR ^= 0x08;
-    vTaskDelay(1000);
-  }
+  TimingDelay = nTime;
+  while(TimingDelay != 0);
 }
 
-static void Led3(void *pvParameters)
+__IO uint32_t micros(void)
 {
-  while(1){
-    // toggle led
-    GPIOC->ODR ^= 0x2000;
-    vTaskDelay(5000);
-  }
+  return Tick;
+}
+
+__IO uint32_t millis(void)
+{
+  return (Tick / 1000);
+}
+
+void StartTimeoutUs(__IO uint32_t nTime)
+{
+  TimeoutUs = nTime;
+}
+
+int IsReachTimeout(void)
+{
+  return (TimeoutUs == 0);
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -148,7 +124,7 @@ static void Led3(void *pvParameters)
   * @retval None
   */
 void assert_failed(uint8_t* file, uint32_t line)
-{
+{ 
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
@@ -168,4 +144,4 @@ void assert_failed(uint8_t* file, uint32_t line)
   * @}
   */
 
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
+/*********************************END OF FILE**********************************/
