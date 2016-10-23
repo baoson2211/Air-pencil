3a66bd8439f7fa52ccc4ab25936f44a156491576
baoson2211 <baoson92@gmail.com>
2016-10-23 23:35:46 +0700

    data struct of imu

--- HW/Sensors/inc/imu.h
+++ HW/Sensors/inc/imu.h
@@ -5,10 +5,21 @@
 /* Includes ------------------------------------------------------------------*/
 #include "bit.h"
 /* Exported types ------------------------------------------------------------*/
+typedef struct __imu {
+  __IO uint8_t *accel_buff;
+  __IO uint16_t accel_x;
+  __IO uint16_t accel_y;
+  __IO uint16_t accel_z;
+
+  __IO uint8_t *gyro_buff;
+  __IO uint16_t gyro_x;
+  __IO uint16_t gyro_y;
+  __IO uint16_t gyro_z;
+} imu;
+
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
-
-
 /* Exported functions ------------------------------------------------------- */
+
 
 #endif /* __IMU_H */
