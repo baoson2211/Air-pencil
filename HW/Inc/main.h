48452e1ed0b132db2662daa7d447f2ffe8fb9e39
baoson2211 <baoson92@gmail.com>
2016-10-23 23:25:04 +0700

    main.h

---
+++ HW/Inc/main.h
@@ -0,0 +1,26 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include "stm32f10x_conf.h"
+#include "usart.h"
+#include "i2c.h"
+#include "ADXL345.h"
+
+__IO uint32_t TimingDelay;
+__IO uint32_t Tick;
+__IO uint32_t TimeoutUs;
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+void DelayUs(__IO uint32_t nTime);
+__IO uint32_t micros(void);
+__IO uint32_t millis(void);
+void StartTimeoutUs(__IO uint32_t nTime);
+int IsReachTimeout(void);
+
+#endif /* __MAIN_H */
